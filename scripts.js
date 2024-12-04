f6b04499be7fbfe3f649ffc9f1f21a00328af03f
kuoleo0903 <kuoleo0903@gmail.com>
2024-12-05 00:08:09 +0800

    20241205

---
+++ scripts.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('nav ul li a');
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const targetPage = this.getAttribute('href');
+            window.location.href = targetPage;
+        });
+    });
+});
